f9561f8f638aafd82d8947888b7858384390f1c3
KeaganChasenski <k.chasenski@gmail.com>
2021-06-10 11:45:07 +0200

    AND_perceptron() function generates testing and validation sets, creates an AND perceptron with a bais of -1.5 and trains itself

--- XOR.py
+++ XOR.py
@@ -15,39 +15,26 @@ from  Perceptron import Perceptron
 num_train = 50 ; 
 learning_rate = 0.2 ; 
 
-def generate_training_set():
+
+def AND_perceptron():
+    print("Training AND Peceptron (Gate 0)...")
     training_examples = []
     training_labels = []
 
     for i in range(num_train):
         training_examples.append([random.random(), random.random()])
         # We want our perceptron to be noise tolerant, so we label all examples where x1 and x2 > 0.8 as 1.0
-        training_labels.append(1.0 if training_examples[i][0] > 0.8 and training_examples[i][1] > 0.8 else 0.0)
-    
-    return training_examples, training_labels 
-
-def generate_validation_set():
+        training_labels.append(1.0 if training_examples[i][0] > 0.75 and training_examples[i][1] > 0.75 else 0.0)
 
     validate_examples = []
     validate_labels = []
 
     for i in range(num_train):
         validate_examples.append([random.random(), random.random()])
-        validate_labels.append(1.0 if validate_examples[i][0] > 0.8 and validate_examples[i][1] > 0.8 else 0.0)
+        validate_labels.append(1.0 if validate_examples[i][0] > 0.75 and validate_examples[i][1] > 0.75 else 0.0)
 
-    return validate_examples, validate_labels
-
-def AND_perceptron():
-    training_examples, training_labels = generate_training_set()
-    validate_examples, validate_labels = generate_validation_set()
-
-    #print(training_examples)
-    #print(training_labels)
-
-    AND = Perceptron(2, bias=-1.0)
-    #print(AND.weights)
+    AND = Perceptron(2, bias=-1.5)
     valid_percentage = 0
-    #print(valid_percentage)ß
 
     i = 0
     while valid_percentage < 0.98: # We want our Perceptron to have an accuracy of at least 80%
@@ -64,5 +51,25 @@ def AND_perceptron():
         if i == 50: 
             break
 
+def OR_perceptron():
+    print("Training OR Peceptron (Gate 1)...")
+    training_examples = []
+    training_labels = []
+
+    for i in range(num_train):
+        training_examples.append([random.random(), random.random()])
+        # We want our perceptron to be noise tolerant, so we label all examples where x1 and x2 > 0.8 as 1.0
+        training_labels.append(1.0 if training_examples[i][0] > 0.75 or training_examples[i][1] > 0.75 else 0.0)
+
+    validate_examples = []
+    validate_labels = []
+
+    for i in range(num_train):
+        validate_examples.append([random.random(), random.random()])
+        validate_labels.append(1.0 if validate_examples[i][0] > 0.75 or validate_examples[i][1] > 0.75 else 0.0)
+
+    print(training_examples, training_labels)
+
 if __name__ == "__main__":
     AND_perceptron()
+    OR_perceptron()
