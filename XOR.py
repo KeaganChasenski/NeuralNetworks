6b3f75d1e0c1ded105853a65407b02247648a62d
KeaganChasenski <k.chasenski@gmail.com>
2021-06-10 11:02:59 +0200

    Assignment 3 started. XOR.py file created and linked to a makefile copied from assignment2. The perceptron class has been copied from code provided on vula.

---
+++ XOR.py
@@ -0,0 +1,16 @@
+# Assignment 3 - CNNS
+    #XOR.py
+
+# Keagan Chasenski
+# CHSKEA001
+# 10 June 2021
+
+#Returns
+    #
+
+import numpy as np
+import sys
+import random
+
+if __name__ == "__main__":
+    print("Hi") 
