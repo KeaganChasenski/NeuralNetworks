2a43ea8c688fbdc689d8f17198c3d98c60084026
KeaganChasenski <k.chasenski@gmail.com>
2021-06-12 17:53:11 +0200

    AND_perceptron updated to random values for training and validation of -.025 -> 1.25 as per tutor testing. Accuracry not 0.98 anymore. ToDo, adjust bias to get and accuracry back. Then implement the same with OR perceptron and get NOT perceptron to work.

--- XOR.py
+++ XOR.py
@@ -7,8 +7,8 @@
 
 #Returns
     # The class (0/1) of two inputs passed by user based of XOR logic gate
-    # Creates XOR peceptron from output of two perceptrons
-    # NAND and OR perceptrons
+    # Creates XOR peceptron from output of 3 perceptrons
+    # AND, NOT and OR perceptrons
 # Inputs 
     # x1 x2 when promted
     # If want to exit, 'exit' 
@@ -25,21 +25,21 @@ def AND_perceptron():
     training_labels = []
 
     for i in range(num_train):
-        training_examples.append([random.random(), random.random()])
+        training_examples.append([random.uniform(-0.25,1.25), random.uniform(-0.25,1.25)])
         # We want our perceptron to be noise tolerant, so we label all examples where x1 and x2 > 0.8 as 1.0
         training_labels.append(1.0 if training_examples[i][0] > 0.75 and training_examples[i][1] > 0.75 else 0.0)
 
     validate_examples = []
     validate_labels = []
-
+    
     for i in range(num_train):
-        validate_examples.append([random.random(), random.random()])
+        validate_examples.append([random.uniform(-0.25,1.25), random.uniform(-0.25,1.25)])
         validate_labels.append(1.0 if validate_examples[i][0] > 0.75 and validate_examples[i][1] > 0.75 else 0.0)
 
+    
     AND = Perceptron(2, bias=-1.5)
     valid_percentage = 0
 
-    print(AND.weights, AND.num_inputs)
     i = 0
     while valid_percentage < 0.98: # We want our Perceptron to have an accuracy of at least 80%
         i += 1
@@ -54,8 +54,8 @@ def AND_perceptron():
         # You shouldn't need to do this as your networks may require much longer to train. 
         if i == num_train: 
             break
-    print("Accuracy = ", valid_percentage, "in ", i , "i terations")
-    return AND 
+    print("Accuracy = ", valid_percentage, "in", i , "iterations")
+    return AND, valid_percentage
 
 def OR_perceptron():
     print("Training OR Peceptron (Gate 1)...")
@@ -74,14 +74,14 @@ def OR_perceptron():
         validate_examples.append([random.random(), random.random()])
         validate_labels.append(1.0 if validate_examples[i][0] > 0.75 or validate_examples[i][1] > 0.75 else 0.0)
 
-    OR = Perceptron(2, bias=-0.5)
+    OR = Perceptron(2, bias=-1)
     valid_percentage = 0
 
     i = 0
     while valid_percentage < 0.98: # We want our Perceptron to have an accuracy of at least 80%
         i += 1
 
-        OR.train(training_examples, training_labels, 0.2)  # Train our Perceptron
+        OR.train(training_examples, training_labels, 0.4)  # Train our Perceptron
         #print('------ Iteration ' + str(i) + ' ------')
         #print(OR.weights)
         valid_percentage = OR.validate(validate_examples, validate_labels, verbose=False) # Validate it
@@ -92,7 +92,7 @@ def OR_perceptron():
         if i == num_train: 
             break
     print("Accuracy = ", valid_percentage, "in ", i , "i terations")
-    return OR
+    return OR, valid_percentage
 
 def NAND_perceptron():
     print("Training NAND Peceptron (Gate 2)...")
@@ -183,11 +183,17 @@ def XOR_perceptron(x1,x2):
 
 if __name__ == "__main__":
     # Construct each perceptron
-    AND = AND_perceptron()
-    #OR = OR_perceptron()
+    AND_accuracy = 0
+    while AND_accuracy < 0.98:
+        AND, AND_accuracy = AND_perceptron()
+
+    OR_accuracy = 0 
+    while OR_accuracy < 0.98:
+        OR, OR_accuracy = OR_perceptron()
+
     #NAND = NAND_perceptron()
 
-    NOT = NOT_perceptron()
+    #NOT = NOT_perceptron()
     
     # Build Network of perceptrons
     print("Constructing Network...")
